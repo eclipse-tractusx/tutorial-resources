090e8f20814fa9e2badc94be1237a4c2a884cac2
KUMAR <ravinder.kumar02@sap.com>
2023-12-11 13:42:58 +0530

    feedback changes

---
+++ mxd/backend-service/src/main/java/org/eclipse/tractusx/mxd/repository/Impl/TransferRepositoryImpl.java
@@ -0,0 +1,144 @@
+
+/*******************************************************************************
+ *
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ ******************************************************************************/
+
+package org.eclipse.tractusx.mxd.repository.Impl;
+
+import org.eclipse.tractusx.mxd.model.TransfersResponse;
+import org.eclipse.tractusx.mxd.util.Constants;
+import org.eclipse.tractusx.mxd.util.HibernateUtil;
+import org.eclipse.tractusx.mxd.entity.Content;
+import org.eclipse.tractusx.mxd.entity.Transfer;
+import org.eclipse.tractusx.mxd.repository.TransferRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.Query;
+import org.hibernate.Session;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+
+@ApplicationScoped
+public class TransferRepositoryImpl implements TransferRepository {
+
+    private static final Logger logger = Logger.getLogger(TransferRepositoryImpl.class.getName());
+
+    @Override
+    public String createTransferWithID(String asset, String contents, String id) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            if (session != null) {
+                Transfer transfer = new Transfer();
+                transfer.setTransferID(id);
+                transfer.setAsset(asset);
+                transfer.setCreatedDate(new Date());
+                transfer.setUpdatedDate(new Date());
+                transfer.setContents(contents);
+                session.getTransaction().begin();
+                session.persist(transfer);
+                session.getTransaction().commit();
+                session.close();
+                if (transfer.getTransferID() != null) {
+                    return transfer.getTransferID();
+                } else {
+                    return Constants.EMPTYSTRING;
+                }
+            }
+
+
+        } catch (Exception e) {
+            logger.info(e.getMessage());
+        }
+        return Constants.EMPTYSTRING;
+    }
+
+    @Override
+    public TransfersResponse getTransferById(String id) {
+        Transfer transfer = new Transfer();
+        TransfersResponse transfersData = new TransfersResponse();
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            if (session != null) {
+                transfer = session.find(Transfer.class, id);
+                logger.info("transferRepoImpl " + transfer);
+                session.close();
+            }
+            transfersData = new TransfersResponse(transfer.getAsset(), transfer.getContents(), transfer.getTransferID(), transfer.getCreatedDate(), transfer.getUpdatedDate());
+        } catch (Exception e) {
+            logger.info(e.getMessage());
+        }
+        return transfersData;
+    }
+
+    @Override
+    public void updateTransfer(Long id, String asset) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            if (session != null) {
+                Transfer transfer = session.find(Transfer.class, id);
+                transfer.setAsset(asset);
+                session.close();
+            }
+
+        } catch (Exception e) {
+            logger.info(e.getMessage());
+        }
+    }
+
+    @Override
+    public void deleteTransfer(Long id) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            if (session != null) {
+                Transfer transfer = session.find(Transfer.class, id);
+                session.getTransaction().begin();
+                session.remove(transfer);
+                session.getTransaction().commit();
+                session.close();
+            }
+
+        } catch (Exception e) {
+            logger.info(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<TransfersResponse> getAllTransfers() {
+        List<TransfersResponse> response = new ArrayList<TransfersResponse>();
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            if (session != null) {
+                Query query = session.createQuery("select e from Content e", Content.class);
+                List<Transfer> transferList = query.getResultList();
+                transferList.forEach(data -> {
+                    TransfersResponse transfersResponse = new TransfersResponse(data.getAsset(), data.getContents(), data.getCreatedDate(), data.getCreatedDate());
+                    response.add(transfersResponse);
+                });
+                session.close();
+            }
+
+        } catch (Exception e) {
+            logger.info(e.getMessage());
+        }
+        return response.isEmpty() ? null : response;
+    }
+}
---
+++ mxd/backend-service/src/main/java/org/eclipse/tractusx/mxd/util/ContentsRandom.java
@@ -0,0 +1,57 @@
+
+/*******************************************************************************
+ *
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ ******************************************************************************/
+
+package org.eclipse.tractusx.mxd.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.tractusx.mxd.model.ContentRandomResponse;
+
+import java.util.Random;
+import java.util.logging.Logger;
+
+@ApplicationScoped
+public class ContentsRandom {
+    private static final Logger logger = Logger.getLogger(ContentsRandom.class.getName());
+
+    public Response getRandomContent() {
+        try {
+            Random random = new Random();
+            int random_number = random.nextInt(100) + 1;
+            String title = RandomWordUtil.generateRandomWord();
+            String text = RandomWordUtil.generateRandomWord();
+            ContentRandomResponse contentRandomResponse = new ContentRandomResponse();
+            contentRandomResponse.setId(random_number);
+            contentRandomResponse.setText(text);
+            contentRandomResponse.setTitle(title);
+            ObjectMapper objectMapper = SingletonObjectMapper.getObjectMapper();
+            return Response.ok(objectMapper.writeValueAsString(contentRandomResponse)).build();
+
+        } catch (Exception e) {
+            logger.info("error " + e.getMessage());
+        }
+
+        return Response.ok("Random Json Not Generated !").build();
+    }
+
+}
