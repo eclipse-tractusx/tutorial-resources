189e3427e167022639ce5f81824db90ceb87d27b
Sebastian Dechant <763247+S3bb1@users.noreply.github.com>
2024-02-07 09:04:35 +0000

    set public url of dataplane to be accessible outside kubernetes

--- mxd/modules/connector/main.tf
+++ mxd/modules/connector/main.tf
@@ -89,6 +89,9 @@ resource "helm_release" "connector" {
         aws : {
           endpointOverride : "http://${local.minio-url}"
         }
+        url: {
+          public: "http://localhost/${var.humanReadableName}/api/public"
+        }
       }
     })
   ]
