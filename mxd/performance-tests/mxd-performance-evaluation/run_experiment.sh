5d450110bead46b24a6f0b1ec832255b21f5bbc6
ciprian-nicuta <158159743+ciprian-nicuta@users.noreply.github.com>
2024-03-28 12:19:52 +0200

    JMeter Scripts to run the performance test against EDC

---
+++ mxd/performance-tests/mxd-performance-evaluation/run_experiment.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+#  Copyright (c) 2024 Contributors to the Eclipse Foundation
+#
+#  See the NOTICE file(s) distributed with this work for additional
+#  information regarding copyright ownership.
+#
+#  This program and the accompanying materials are made available under the
+#  terms of the Apache License, Version 2.0 which is available at
+#  https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+#
+#  SPDX-License-Identifier: Apache-2.0
+#
+
+experiment_property="mount/custom_experiment.properties"
+
+if [ -z "$JMETER_HOME" ]; then
+  echo "JMETER_HOME is not defined - please define it."
+  exit 1  
+fi
+echo "Jmeter found at path $JMETER_HOME"
+
+jmeter_binary="$JMETER_HOME/bin/jmeter"
+
+rm -rf output
+mkdir -p output/dashboard
+
+rm -rf output_slim
+mkdir -p output_slim/dashboard
+
+echo "*** Property: $experiment_property Start ***"
+cat $experiment_property
+echo "*** Property: $experiment_property End ***"
+
+echo "*** Performance Test Start ***"
+if [ -z "$jmeter_script" ]; then
+  echo "Executing all scripts in order"
+  echo "Executing setup.jmx"
+  $jmeter_binary -n -t mount/setup.jmx -l output/setup.jtl -q $experiment_property -j "output/jmeter.log"
+  echo "Executing measurement_interval.jmx"
+  $jmeter_binary -n -t mount/measurement_interval.jmx -l output/measurement_interval.jtl -q $experiment_property \
+    -j "output/jmeter.log" -e -o output/dashboard
+fi
+echo "*** Performance Test End ***"
+
+echo "Copying custom_experiment.properties to output folder with new name metadata.txt"
+cp $experiment_property output/metadata.txt
+echo "Creating tar file with output"
+tar -cf output.tar output
+
+echo "Copying custom_experiment.properties to output_slim folder with new name metadata.txt"
+cp $experiment_property output_slim/metadata.txt
+echo "Copying output/dashboard/statistics.json to output_slim/dashboard/statistics.json folder"
+cp output/dashboard/statistics.json output_slim/dashboard/statistics.json
+echo "Creating tar file with output"
+tar -cf output_slim.tar output_slim
+
+echo "*** Test Completed..., Sleeping now ***"
+while true; do sleep 10000; done
