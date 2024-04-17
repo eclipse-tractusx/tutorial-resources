954b15f948613fd9395e4d3e960015928d4af12e
ciprian-nicuta <158159743+ciprian-nicuta@users.noreply.github.com>
2024-04-17 14:48:12 +0300

    Enable keep alive in JMeter files

--- mxd/performance-tests/mxd-performance-evaluation/run_experiment.sh
+++ mxd/performance-tests/mxd-performance-evaluation/run_experiment.sh
@@ -22,7 +22,7 @@ experiment_property="mount/custom_experiment.properties"
 
 if [ -z "$JMETER_HOME" ]; then
   echo "JMETER_HOME is not defined - please define it."
-  exit 1  
+  exit 1
 fi
 echo "Jmeter found at path $JMETER_HOME"
 
@@ -34,6 +34,9 @@ mkdir -p output/dashboard
 rm -rf output_slim
 mkdir -p output_slim/dashboard
 
+rm -rf output_jtl
+mkdir -p output_jtl
+
 echo "*** Property: $experiment_property Start ***"
 cat $experiment_property
 echo "*** Property: $experiment_property End ***"
@@ -42,10 +45,10 @@ echo "*** Performance Test Start ***"
 if [ -z "$jmeter_script" ]; then
   echo "Executing all scripts in order"
   echo "Executing setup.jmx"
-  $jmeter_binary -n -t mount/setup.jmx -l output/setup.jtl -q $experiment_property -j "output/jmeter.log"
+  $jmeter_binary -n -t mount/setup.jmx -l output_jtl/setup.jtl -q $experiment_property -j "output/setup_jmeter.log"
   echo "Executing measurement_interval.jmx"
-  $jmeter_binary -n -t mount/measurement_interval.jmx -l output/measurement_interval.jtl -q $experiment_property \
-    -j "output/jmeter.log" -e -o output/dashboard
+  $jmeter_binary -n -t mount/measurement_interval.jmx -l output_jtl/measurement_interval.jtl -q $experiment_property \
+    -j "output/measurement_interval_jmeter.log" -e -o output/dashboard
 fi
 echo "*** Performance Test End ***"
 
@@ -60,6 +63,8 @@ echo "Copying output/dashboard/statistics.json to output_slim/dashboard/statisti
 cp output/dashboard/statistics.json output_slim/dashboard/statistics.json
 echo "Creating tar file with output"
 tar -cf output_slim.tar output_slim
+
+tar -czf output_jtl.tar.gz output_jtl
 
 echo "*** Test Completed..., Sleeping now ***"
 while true; do sleep 10000; done
