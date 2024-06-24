07b807bd0351b8435159a8415260a5d6a07a398e
ieuna <ieuna@sap.com>
2024-06-24 10:50:29 +0300

    Fixed terraform file format

--- mxd/postgres-init.tf
+++ mxd/postgres-init.tf
@@ -56,10 +56,10 @@ resource "kubernetes_config_map" "postgres-initdb-config" {
 }
 
 locals {
-  keycloak-postgres = var.common-postgres-instance ? module.common-postgres[0] : module.postgres["keycloak"]
-  miw-postgres      = var.common-postgres-instance ? module.common-postgres[0] : module.postgres["miw"]
-  alice-postgres    = var.common-postgres-instance ? module.common-postgres[0] : module.postgres["alice"]
-  bob-postgres      = var.common-postgres-instance ? module.common-postgres[0] : module.postgres["bob"]
+  keycloak-postgres       = var.common-postgres-instance ? module.common-postgres[0] : module.postgres["keycloak"]
+  miw-postgres            = var.common-postgres-instance ? module.common-postgres[0] : module.postgres["miw"]
+  alice-postgres          = var.common-postgres-instance ? module.common-postgres[0] : module.postgres["alice"]
+  bob-postgres            = var.common-postgres-instance ? module.common-postgres[0] : module.postgres["bob"]
   backendservice-postgres = var.common-postgres-instance ? module.common-postgres[0] : module.postgres["backendservice"]
 
   databases = {
